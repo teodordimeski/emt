cf1f541e310a432c998418ceafc1a78040576692
teodordimeski <teodordimeski@gmail.com>
2026-03-14 13:06:41 +0100

    initial commit

---
+++ labs/lab1_groupB/src/test/java/mk/ukim/finki/emt/lab1_groupb/Lab1GroupBApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.lab1_groupb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1GroupBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
