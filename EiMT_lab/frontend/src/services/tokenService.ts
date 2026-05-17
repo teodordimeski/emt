e61d51d49db1a91339610d8ee113ca5eb4766b28
teodordimeski <teodordimeski@gmail.com>
2026-05-17 16:13:55 +0200

    removes unused files

--- EiMT_lab/frontend/src/services/tokenService.ts
+++
@@ -1,8 +0,0 @@
-// Store and manage JWT tokens in localStorage
-export const tokenService = {
-  getToken: () => localStorage.getItem('token'),
-  setToken: (token: string) => localStorage.setItem('token', token),
-  removeToken: () => localStorage.removeItem('token'),
-  isTokenValid: () => !!localStorage.getItem('token')
-};
-
